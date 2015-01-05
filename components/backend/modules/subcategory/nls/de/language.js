d2e1a9467664fd9ebe58254403662efdc53c5b12
dni <dni.khr@gmail.com>
2015-01-05 12:27:13 +0100

    subcategorymodule

---
+++ components/backend/modules/subcategory/nls/de/language.js
@@ -0,0 +1,13 @@
+define({
+	"details": "Subkategorie Details",
+	"navigation": "Subkategorien",
+	"emptyMessage": "Die Subkategorie existiert nicht mehr",
+	"newModel": "hat Subkategorie erstellt",
+	"deleteModel": "hat Subkategorie gelöscht",
+	"updateModel": "hat Subkategorie aktualisiert",
+	"attributes": {
+        "title": "Titel",
+        "size": "Größe",
+        "symbol": "Symbol"
+    }
+});
---
+++ components/backend/modules/subcategory/nls/language.js
@@ -0,0 +1,16 @@
+define({
+    "root": {
+    	"details": "Subcategory details",
+    	"navigation": "Subcategories",
+      "emptyMessage": "The subcategory does not exist anymore",
+      "newModel": "created a new subcategory",
+      "deleteModel": "deleted subcategory",
+      "updateModel": "updated subcategory",
+      "attributes": {
+        "title": "Title",
+        "size": "Size",
+        "symbol": "Symbol"
+      }
+    },
+    "de": true
+});
